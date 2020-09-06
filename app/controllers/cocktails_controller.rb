a5487fc7c345d09fc0f276f59023f23061bfb885
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-09-06 18:14:03 +1000

    Added controllers, views & seeds

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,41 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to @cocktail, notice: "Cocktail was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to @cocktail, notice: "Cocktail was successfully deleted."
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "Create ingredients"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "strawberry")
+# Ingredient.create(name: "pineapple")
+# Ingredient.create(name: "coconut")
+
+require 'faker'
+
+# puts "Destroy ingredients"
+# Ingredient.destroy_all if Rails.env.development?
+
+# puts "Destroy Cocktails"
+# Cocktail.destroy_all if Rails.env.development?
+
+puts "Create ingredients"
+
+10.times do
+  ingredient = Ingredient.create(
+    name: Faker::Food.ingredient
+  )
+  ingredient.save!
+end
+puts 'Finished!'
+
+
